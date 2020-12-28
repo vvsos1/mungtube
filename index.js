6ef39d2ae5aa095ec37872cac65dc426c2c40ed2
vvsos1 <vvsos10@naver.com>
2020-12-28 15:04:43 +0900

    add middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 const app = express();
 
 const PORT = 80;
@@ -7,12 +12,18 @@ function handleListening() {
     console.log(`Server Running On http://localhost:${PORT}`);
 }
 
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    res.send('Hello from home');
+    res.send(`Hello from home`);
 })
 
 app.get('/profile', (req, res) => {
-    res.send('Hello from profile')
+    res.send(`Hello from profile`)
 })
 
 app.listen(80,handleListening);
