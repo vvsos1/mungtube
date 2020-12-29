b6cb3de40a354afa62775a960e62c16e370e7571
vvsos1 <vvsos10@naver.com>
2020-12-29 15:24:53 +0900

    add routers

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import userRouter from './routers/userRouter';
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
 
 app.use(helmet());
 app.use(cookieParser());
@@ -14,14 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.send(`Hello from home`);
-});
-
-app.get('/profile', (req, res) => {
-    res.send(`Hello from profile`)
-});
-
+app.use('/',globalRouter);
+app.use('/video',videoRouter);
 app.use('/user',userRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
 
 userRouter.get('/',(req,res) => res.send('user home'));
 userRouter.get('/edit',(req,res) => res.send('user edit'));
 userRouter.get('/changePassword',(req,res) => res.send('user changePassword'));
 
+export default userRouter;
