093565b4069c4085369c7dd670dd5fab8cba9e11
vvsos1 <vvsos10@naver.com>
2020-12-30 14:48:15 +0900

    add localsMiddleware

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import userRouter from './routers/userRouter';
-import globalRouter from './routers/globalRouter';
-import videoRouter from './routers/videoRouter';
-import routes from './routes';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
-
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(morgan('dev'));
-app.set('view engine','pug');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.set("view engine", "pug");
+app.use(localsMiddleware);
 
-app.use(routes.home,globalRouter);
-app.use(routes.videos,videoRouter);
-app.use(routes.users,userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.videos, videoRouter);
+app.use(routes.users, userRouter);
 
 export default app;
