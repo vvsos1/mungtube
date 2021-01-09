27ea3638e71e8c524b747f4a1517aa384dd93d1b
vvsos1 <vvsos10@naver.com>
2021-01-09 23:13:30 +0900

    add webpack

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import "../scss/style.scss";
+
+const something = async () => {
+  console.log("sometihing");
+};
--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.use(morgan("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(localsMiddleware);
 
 app.use((req, res, next) => {
