d704f92664efd03898d9403589c9a0833a54bc4c
vvsos1 <vvsos10@naver.com>
2021-02-17 22:06:28 +0900

    #8.2

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,13 +1,29 @@
 const videoContainer = document.querySelector("#jsVideoPlayer");
 const videoPlayer = videoContainer?.querySelector("video");
-const playBtn = videoContainer?.querySelector("#jsPlayButton");
 
-function handlePlayClick() {
+const playBtn = videoContainer?.querySelector("#jsPlayButton");
+const playIcon = playBtn?.querySelector(".play-icon");
+const pauseIcon = playBtn?.querySelector(".pause-icon");
+
+const volumeBtn = videoContainer?.querySelector("#jsVolumeButton");
+const mutedIcon = volumeBtn?.querySelector(".muted-icon");
+const unmutedIcon = volumeBtn?.querySelector(".unmuted-icon");
+
+function toggleVideoPlay() {
   if (videoPlayer?.paused) {
     videoPlayer?.play();
   } else {
     videoPlayer?.pause();
   }
+  playIcon?.classList?.toggle("invisible");
+  pauseIcon?.classList?.toggle("invisible");
 }
 
-playBtn?.addEventListener("click", handlePlayClick);
+function toggleVideoMute() {
+  videoPlayer.muted = !videoPlayer.muted;
+  mutedIcon?.classList?.toggle("invisible");
+  unmutedIcon?.classList?.toggle("invisible");
+}
+
+playBtn?.addEventListener("click", toggleVideoPlay);
+volumeBtn?.addEventListener("click", toggleVideoMute);
