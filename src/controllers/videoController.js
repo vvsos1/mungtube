29459e0e49e630bff1711ec19c2c53f11c21e697
vvsos1 <vvsos10@naver.com>
2021-02-17 19:55:36 +0900

    #8.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -50,7 +50,7 @@ export const videoDetail = async (req, res) => {
   try {
     const video = await Video.findById(id).populate("creator");
     res.render("videoDetail", { pageTitle: video.title, video });
-  } catch (erorr) {
+  } catch (error) {
     res.redirect(routes.home);
   }
 };
