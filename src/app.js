ab4c0948aa7f2c5929751157256ab3a84b11f150
vvsos1 <vvsos10@naver.com>
2021-02-17 21:20:11 +0900

    resolve Content-Security-Policy problem

--- src/app.js
+++ src/app.js
@@ -20,8 +20,19 @@ import "./passport";
 const app = express();
 
 const CookieStore = MongoStore(session);
-
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["*"],
+      scriptSrc: ["unsafe-eval", "localhost"],
+    },
+  })
+);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,14 +53,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(localsMiddleware);
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' https://archive.org"
-  );
-  return next();
-});
 
 app.use(routes.home, globalRouter);
 app.use(routes.videos, videoRouter);
